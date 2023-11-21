288efd6c938af86397b5a79ec902ed227a06965d
c0a22085 <c0a22085de@edu.teu.ac.jp>
2023-11-21 16:57:09 +0900

    課題2

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg") #練習1
+    bg_imgs = [bg_img, pg.transform.flip(bg_img, True, False)]*2
+
     kk_img = pg.image.load("ex01/fig/3.png") #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
     kk_imgs = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)] #練習3
@@ -16,8 +18,10 @@ def main():
             if event.type == pg.QUIT: return
 
         x = tmr%1600
-        screen.blit(bg_img, [-x, 0]) #練習4
-        screen.blit(bg_img, [1600-x, 0])
+        for i in range(4):
+            screen.blit(bg_imgs[i], [1600*i-x, 0])
+        #screen.blit(bg_img, [-x, 0]) #練習4
+        #screen.blit(bg_img, [1600-x, 0])
         screen.blit(kk_imgs[tmr%100//50], [300, 200]) #練習5
         pg.display.update()
         tmr += 1       
